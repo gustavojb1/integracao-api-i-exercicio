3e84851678ab5588f40dfc581d3b630536d1d3a9
Gustavo Junqueira Barbosa <gustavojunqueirab@gmail.com>
2022-11-17 16:41:04 -0300

    finished

--- src/componentes/Musicas/Musicas.js
+++ src/componentes/Musicas/Musicas.js
@@ -1,46 +1,130 @@
-import React, { useState } from 'react'
-import { Botao, ContainerInputs, ContainerMusicas, InputMusica, Musica } from './styled'
+import React, { useEffect, useState } from "react";
+import {
+  Botao,
+  ContainerInputs,
+  ContainerMusicas,
+  InputMusica,
+  Musica,
+} from "./styled";
+import axios from "axios";
 
-const musicasLocal = [{
+const musicasLocal = [
+  {
     artist: "Artista 1",
     id: "1",
     name: "Musica1",
-    url: "http://spoti4.future4.com.br/1.mp3"
-},
-{
+    url: "http://spoti4.future4.com.br/1.mp3",
+  },
+  {
     artist: "Artista 2",
     id: "2",
     name: "Musica2",
-    url: "http://spoti4.future4.com.br/2.mp3"
-},
-{
+    url: "http://spoti4.future4.com.br/2.mp3",
+  },
+  {
     artist: "Artista 3",
     id: "3",
     name: "Musica3",
-    url: "http://spoti4.future4.com.br/3.mp3"
-}]
+    url: "http://spoti4.future4.com.br/3.mp3",
+  },
+];
 
 export default function Musicas(props) {
-    const [musicas, setMusicas] = useState(musicasLocal)
+  const [musicas, setMusicas] = useState(null);
+  const [artista, setArtista] = useState("");
+  const [music, setMusic] = useState("");
+  const [url, setUrl] = useState("");
 
-    return (
-        <ContainerMusicas>
-            <h2>{props.playlist.name}</h2>
-            {musicas.map((musica) => {
-                return (
-                    <Musica key={musica.id}>
-                        <h3>{musica.name} - {musica.artist}</h3>
-                        <audio src={musica.url} controls />
-                        <button>X</button>
-                    </Musica>)
-            })}
-            <ContainerInputs>
-                <InputMusica placeholder="artista" />
-                <InputMusica placeholder="musica" />
-                <InputMusica placeholder="url" />
-                <Botao>Adicionar musica</Botao>
-            </ContainerInputs>
-        </ContainerMusicas>
-    )
+  const pegarMusicas = () => {
+    axios
+      .get(
+        `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${props.playlist.id}/tracks`,
+        { headers: { Authorization: "gustavo-barbosa-ammal" } }
+      )
+      .then((response) => {
+        setMusicas(response.data.result.tracks);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    pegarMusicas();
+  }, []);
+
+  const add = () => {
+    axios
+      .post(
+        `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${props.playlist.id}/tracks`,
+        {
+          name: music,
+          artist: artista,
+          url: url,
+        },
+        { headers: { Authorization: "gustavo-barbosa-ammal" } }
+      )
+      .then((response) => {
+        pegarMusicas();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const del = (musicaId) => {
+    axios
+      .delete(
+        `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${props.playlist.id}/tracks/${musicaId}`,
+        { headers: { Authorization: "gustavo-barbosa-ammal" } }
+      )
+      .then((response) => {
+        pegarMusicas();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <ContainerMusicas>
+      <h2>{props.playlist.name}</h2>
+      {musicas &&
+        musicas.map((musica) => {
+          return (
+            <Musica key={musica.id}>
+              <h3>
+                {musica.name} - {musica.artist}
+              </h3>
+              <audio src={musica.url} controls />
+              <button onClick={()=>(del(musica.id))}>X</button>
+            </Musica>
+          );
+        })}
+      <ContainerInputs>
+        <InputMusica
+          placeholder="artista"
+          value={artista}
+          onChange={({ target }) => {
+            setArtista(target.value);
+          }}
+        />
+        <InputMusica
+          placeholder="musica"
+          value={music}
+          onChange={({ target }) => {
+            setMusic(target.value);
+          }}
+        />
+        <InputMusica
+          placeholder="url"
+          value={url}
+          onChange={({ target }) => {
+            setUrl(target.value);
+          }}
+        />
+        <Botao onClick={add}>Adicionar musica</Botao>
+      </ContainerInputs>
+    </ContainerMusicas>
+  );
 }
-
--- src/componentes/Playlists/Playlists.js
+++ src/componentes/Playlists/Playlists.js
@@ -1,35 +1,53 @@
-import React, {  useState } from "react";
+import React, { useEffect, useState } from "react";
 import Musicas from "../Musicas/Musicas";
+import axios from "axios";
 
 const playlistsLocal = [
-    {
-        id: 1,
-        name: "Playlist 1"
-    },
-    {
-        id: 2,
-        name: "Playlist 2"
-    },
-    {
-        id: 3,
-        name: "Playlist 3"
-    },
-    {
-        id: 4,
-        name: "Playlist 4"
-    },
-]
+  {
+    id: 1,
+    name: "Playlist 1",
+  },
+  {
+    id: 2,
+    name: "Playlist 2",
+  },
+  {
+    id: 3,
+    name: "Playlist 3",
+  },
+  {
+    id: 4,
+    name: "Playlist 4",
+  },
+];
 function Playlists() {
-    const [playlists, setPlaylists] = useState(playlistsLocal)
-  
-    return (
-        <div>
-            {playlists.map((playlist) => {
-                return <Musicas key={playlist.id} playlist={playlist}/>
-            })}
+  const [playlists, setPlaylists] = useState(null);
 
-        </div>
-    );
+  const pegarPlaylist = () => {
+    axios
+      .get(
+        "https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists",
+        { headers: { Authorization: "gustavo-barbosa-ammal" } }
+      )
+      .then((response) => {
+        setPlaylists(response.data.result.list);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  useEffect(() => {
+    pegarPlaylist();
+  }, []);
+
+
+  return (
+    <div>
+      {playlists&&playlists.map((playlist) => {
+        return <Musicas key={playlist.id} playlist={playlist} />;
+      })}
+    </div>
+  );
 }
 
 export default Playlists;
